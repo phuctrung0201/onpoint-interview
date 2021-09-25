1389ca53f88d95d602adb708f06d52dc6020e062
bptrung <trung.bp@neo-lab.vn>
2021-09-25 23:49:22 +0700

    feat: implement data on UI

--- lib/shopee_crawler_web/controllers/page_controller.ex
+++ lib/shopee_crawler_web/controllers/page_controller.ex
@@ -1,9 +1,12 @@
 defmodule ShopeeCrawlerWeb.PageController do
   use ShopeeCrawlerWeb, :controller
-  @items_list_enpoint "https://shopee.vn/api/v4/search/search_items?by=pop&limit=30&match_id=88201679&newest=0&order=desc&page_type=shop&scenario=PAGE_OTHERS&version=2"
+
   @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
   def index(conn, _params) do
-    items = Crawler.fetch(@items_list_enpoint)
+    item_limit="60"
+    items_list_endpoint = "https://shopee.vn/api/v4/search/search_items?by=pop&limit=#{item_limit}&match_id=88201679&newest=0&order=desc&page_type=shop&scenario=PAGE_OTHERS&version=2"
+    items = Crawler.fetch(items_list_endpoint)
+
     render(conn, "index.html", items: items)
   end
 end
